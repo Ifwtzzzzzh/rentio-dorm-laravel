abbbfd9b75e2a4b94903bf3c8f790fb7df129702
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-10-12 07:47:24 +0700

    style: styling detail boarding house interface

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class,'index'])->name('home');
 Route::get('/category/{slug}', [CategoryController::class,'show'])->name('category.show');
 Route::get('/city/{slug}', [CityController::class,'show'])->name('city.show');
+Route::get('/kos/{slug}', [BoardingHouseController::class,'show'])->name('kos.show');
 Route::get('/find-kos', [BoardingHouseController::class,'find'])->name('find-kos');
 Route::get('/check-booking', [BookingController::class,'check'])->name('check-booking');
