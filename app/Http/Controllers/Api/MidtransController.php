e43204ef237baa91b360f39f094648d2771e9309
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-10-26 19:07:20 +0700

    feat: add twilio configuration

--- app/Http/Controllers/Api/MidtransController.php
+++ app/Http/Controllers/Api/MidtransController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Transaction;
+use Twilio\Rest\Client;
+
 class MidtransController extends Controller
 {
     public function callback(Request $request) {
@@ -21,6 +23,19 @@ class MidtransController extends Controller
             return response()->json(['message' => 'Transaction not found'],404);
         }
 
+        $sid    = "ACc23363b4e0a8b21da51391c408eb1134";
+        $token  = "442fa1129ecaa229dd4c8153461c3fd5";
+        $twilio = new Client($sid, $token);
+        $messages =
+            "Halo, " . $transaction->name . "!" . PHP_EOL . PHP_EOL .
+            "Kami telah menerima pembayaran Anda dengan kode booking: " . $transaction->code . "." . PHP_EOL .
+            "Total pembayaran: Rp" . number_format($transaction->total_amount, 0, ',', '.') . PHP_EOL . PHP_EOL .
+            "Anda bisa datang ke kos: " . $transaction->boardingHouse->name . PHP_EOL .
+            "Alamat: " . $transaction->boardingHouse->address . PHP_EOL .
+            "Mulai tanggal: " . date('d-m-Y', strtotime($transaction->start_date)) . PHP_EOL . PHP_EOL .
+            "Terima kasih atas kepercayaan Anda!" . PHP_EOL .
+            "Kami tunggu kedatangan Anda.";
+
         switch ($transactionStatus) {
             case 'capture':
                 if ($request->payment_type == 'credit_card') {
@@ -33,6 +48,13 @@ class MidtransController extends Controller
                 break;
             case 'settlement':
                 $transaction->update(['payment_status' => 'success']);
+                $twilio->messages
+                    ->create(
+                        "whatsapp:+" . $transaction->phone_number,
+                        array(
+                            "from" => "whatsapp:+6285839004774",
+                            "body" => $messages
+                    ));
                 break;
             case 'pending':
                 $transaction->update(['payment_status' => 'pending']);
